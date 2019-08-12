4040c1acd017cc74f2a9c41f71a24815c2e3c629
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-08-11 22:29:47 -0700

    initial work
    backup

--- app/src/main/java/com/example/pong/PlayerName.java
+++ app/src/main/java/com/example/pong/PlayerName.java
@@ -1,14 +1,17 @@
 package com.example.pong;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class PlayerName extends AppCompatActivity {
+  //public static final String EXTRA_MESSAGE = "com.example.pong.MESSAGE";
+
     private Button button;
+   // TextView PlayerNameValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +19,44 @@ public class PlayerName extends AppCompatActivity {
         setContentView(R.layout.activity_player_name);
 
         button = findViewById(R.id.submitPlayerNameButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener( ) {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                openLevelOne();
+              //  String playerName = PlayerNameValue.getText( ).toString( );
+              //  Intent startButtonIntent;
+              //  startButtonIntent = new Intent(getApplicationContext( ), LevelOne.class);
+              //  startButtonIntent.putExtra("playerName", playerName);
+              //  sendMessage(view);
+              //  startActivity(startButtonIntent);
+                openLevelOne( );
+
 
             }
         });
+    }
 
+
+  /* public void playerName(){
+       String playerName = PlayerNameValue.getText().toString( );
+       TextView playerName = findViewById(R.id.PlayerNameValue);
+
+   } ;*/
+
+   public void openLevelOne(){
+        Intent intent = new Intent(this, LevelOne.class);
+
+       // TextView playerName = findViewById(R.id.PlayerNameValue);
+       // String name = PlayerNameValue.getText().toString();
+       // intent.putExtra("name", name);
+       startActivity(intent);
     }
-    public void openLevelOne() {
+    
+    /*public void openLevelOne() {
         Intent intent = new Intent(getApplicationContext(),LevelOne.class);
+        TextView PlayerNameValue = (TextView) findViewById(R.id.PlayerNameValue);
+        String gamePlayer = PlayerNameValue.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, gamePlayer);
         startActivity(intent);
-    }
+    }*/
 }
